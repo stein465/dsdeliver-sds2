cd0ee2e015bc56b359840ac8f3e3ca5f1ba26334
Igor Teixeira <igorxdev@gmail.com>
2021-01-08 20:46:57 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,19 @@
 import { useEffect } from 'react';
 import './App.css';
+import Navbar from './Navbar';
 
 
 function App() {
+ 
 
-  <div>HELLO!</div>
+  return(
+
+    <div>
+      <Navbar/>
+    </div>
+
+  );
+  
   
 }
 
